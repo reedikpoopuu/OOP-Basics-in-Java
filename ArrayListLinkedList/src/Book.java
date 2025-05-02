a6800e45cd6da8bfb444a1474a03f6dbf80e44c9
reedikpoopuu <reedik.poopuu@gmail.com>
2025-05-02 21:03:02 +0300

    Create Book class

---
+++ ArrayListLinkedList/src/Book.java
@@ -0,0 +1,54 @@
+public class Book {
+    private String bookTitle;
+    private String author;
+    private int publicationYear;
+    private boolean isAvailable;
+
+    public Book(String bookTitle, String author, int publicationYear) {
+        this.bookTitle = bookTitle;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isAvailable = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + bookTitle + "\nAuthor: " + author + "\nYear: "
+                + publicationYear + "Status: " + isAvailable;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setIsAvailable(boolean status) {
+        this.isAvailable = status;
+    }
+
+    public boolean checkOut() {
+        if (isAvailable) {
+            isAvailable = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean returnBook() {
+        if (!isAvailable) {
+            isAvailable = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
